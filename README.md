65013d054294646d766b7e00775b61966d98f5ad
PranavDarshan <pranav_darshan@yahoo.co.in>
2025-07-03 22:25:55 +0530

    Add Arduino code for ESP8266 BLYNK

---
+++ README.md
@@ -0,0 +1,23 @@
+# NodeMCU Delivery System
+We created a delivery system which could be controlled by the internet. It consisted of a robotic arm and a car. We programmed the NODEMCU and made connections such that the car as well as the robotic arm could be easily controlled by the Blynk platform from anywhere in the world. This could be the stepping stone to creating a completely autonomous last mile delivery system. Also we used OpenCV and Image processing to integrate with our dynamic inventory and tracking system.
+NodeMCU based IoT end to end delivery system that can be easily controlled from any part of the world with Blynk application.
+
+# Pictures
+
+<p align="center">
+  <img src = "https://github.com/PranavDarshan/ESP8266-IoT-Delivery-Vehicle/blob/main/assets/car.jpg" width=400 height=400 align="center"/>
+  <img src = "https://github.com/PranavDarshan/ESP8266-IoT-Delivery-Vehicle/blob/main/assets/robotic_arm.png" width=400 height=400 align="center"/>
+  <img src = "https://github.com/PranavDarshan/ESP8266-IoT-Delivery-Vehicle/blob/main/assets/circuit.png" width=400 height=400 align="center"/>
+</p>
+
+# Circuit Diagram
+
+<p align="center">
+  <img src = "https://github.com/PranavDarshan/ESP8266-IoT-Delivery-Vehicle/blob/main/assets/circuit_diagram.png" width=400 height=400 align="center"/>
+</p>
+
+# More Details
+
+1. Project Report
+
+[ADS REPORT.pdf](https://github.com/PranavDarshan/ESP8266-IoT-Delivery-Vehicle/blob/main/assets/ADS.REPORT.pdf)
---
+++ main.ino
@@ -0,0 +1,155 @@
+/*
+ * you need to select the board as NodeMCU 1.0(ESP12E Module). for this syou can watch our other tutorials in our channel. 
+ * you can also take the help from this link. and also select the COM port to uplaod the program. 
+ */
+// Fill-in information from your Blynk Template here
+#define BLYNK_TEMPLATE_ID "TMPL3UJinHw1e"
+#define BLYNK_DEVICE_NAME "AutonomousDeliveryVehicle"
+
+#define BLYNK_FIRMWARE_VERSION        "0.1.0"
+#define BLYNK_PRINT Serial
+#define USE_NODE_MCU_BOARD
+
+#include "BlynkEdgent.h"
+#include<Servo.h>
+#define servo1 D2
+#define servo2 D5
+#define servo3 D1
+#define servo4 D5
+
+#define ENA D6
+#define IN1 D7
+#define IN2 D8
+#define IN3 D3
+#define IN4 D0
+#define ENB D4
+
+
+Servo mservo1, mservo2, mservo3, mservo4;
+int x = 50;
+int y = 50;
+int Speed=255;
+int speed_Coeff = 1;
+BLYNK_WRITE(V0)
+{
+  int s0 = param.asInt(); 
+  mservo1.write(s0);
+ 
+}
+BLYNK_WRITE(V1)
+{
+  int s0 = param.asInt(); 
+  mservo2.write(s0);
+ 
+}
+BLYNK_WRITE(V2)
+{
+  int s0 = param.asInt(); 
+  mservo3.write(s0);
+ 
+}
+BLYNK_WRITE(V3)
+{
+  int s0 = param.asInt(); 
+  mservo4.write(s0);
+ 
+}
+// Get the joystick values
+BLYNK_WRITE(V4) {
+  x = param[0].asInt();
+}
+// Get the joystick values
+BLYNK_WRITE(V5) {
+  y = param[0].asInt();
+}
+//Get the slider values
+
+
+void smartcar() {
+  if (y > 70) {
+    carForward();
+    Serial.println("carForward");
+  } else if (y < 30) {
+    carBackward();
+    Serial.println("carBackward");
+  } else if (x < 30) {
+    carLeft();
+    Serial.println("carLeft");
+  } else if (x > 70) {
+    carRight();
+    Serial.println("carRight");
+  } else if (x < 70 && x > 30 && y < 70 && y > 30) {
+    carStop();
+    Serial.println("carstop");
+  }
+}
+
+
+void carForward() {
+ analogWrite(ENA, Speed);
+  analogWrite(ENB, Speed);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+ 
+
+}
+void carBackward() {
+ analogWrite(ENA, Speed);
+  analogWrite(ENB, Speed);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+ 
+
+}
+
+void carLeft() {
+  analogWrite(ENA, 0);
+  analogWrite(ENB, Speed);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  
+  
+}
+void carRight() {
+  analogWrite(ENA, Speed);
+  analogWrite(ENB, 0);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+void carStop() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+}
+void setup()
+{
+  Serial.begin(9600);
+  mservo4.attach(servo1);
+  mservo2.attach(servo2); 
+  mservo3.attach(servo3); 
+  mservo4.attach(servo4);
+
+  pinMode(ENA, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(ENB, OUTPUT);
+  BlynkEdgent.begin();
+  delay(1000); 
+}
+
+void loop() 
+{
+  BlynkEdgent.run();
+  smartcar();
+}
